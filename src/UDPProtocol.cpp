5a8ee374246f57aa7875969484b634c5bc018120
silviu caragea <silviu.cpp@gmail.com>
2026-03-31 13:00:06 +0300

    Fix gelflib

--- src/UDPProtocol.cpp
+++ src/UDPProtocol.cpp
@@ -2,19 +2,24 @@
 #include "Data.hpp"
 
 #if defined(WIN32)
-#define NOMINMAX
-#include <winsock.h>
+    #define NOMINMAX
+    #include <winsock.h>
+    using send_result_t = int;
 #elif defined(__linux__) || defined(__APPLE__)
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#define SOCKET int
-#define closesocket(x) close(x)
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <unistd.h>
+    #define SOCKET int
+    #define INVALID_SOCKET -1
+    #define SOCKET_ERROR -1
+    #define closesocket(x) close(x)
+    using send_result_t = ssize_t;
 #endif
 
 #include <algorithm>
 
-bool gelf::UDPProtocol::send( const struct sockaddr_in& pSockAddr, const ChunkedData & rData )
+bool gelf::UDPProtocol::send(const struct sockaddr_in& pSockAddr, const ChunkedData& rData)
 {
     if( rData.m_pData == nullptr || rData.m_uSize == 0 )
     {
@@ -22,37 +27,38 @@ bool gelf::UDPProtocol::send( const struct sockaddr_in& pSockAddr, const Chunked
         return false;
     }
 
-    SOCKET sock;
+    SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
 
-    if( ( sock = socket( AF_INET, SOCK_DGRAM, 0 ) ) == -1 )
+    if(sock == INVALID_SOCKET)
     {
         m_strError = "Could not create socket";
         return false;
     }
 
     size_t uTotalBytesSent = 0;
-    uint8_t* pData = rData.m_pData;
+    const uint8_t* pData = rData.m_pData;
 
-    while( uTotalBytesSent < rData.m_uSize )
+    while(uTotalBytesSent < rData.m_uSize)
     {
-        int iBytesToSend = static_cast< int >( std::min( rData.m_uChunkSize, rData.m_uSize - uTotalBytesSent ) );
-        int iBytesSent = sendto( sock, reinterpret_cast< const char* >( pData ), iBytesToSend, 0, ( struct sockaddr * )&pSockAddr, sizeof( struct sockaddr ) );
+        const size_t uBytesToSend = std::min(rData.m_uChunkSize, rData.m_uSize - uTotalBytesSent);
+        const send_result_t iBytesSent = sendto(sock, reinterpret_cast<const char*>(pData), static_cast<int>(uBytesToSend), 0, reinterpret_cast<const struct sockaddr*>(&pSockAddr), static_cast<int>(sizeof(struct sockaddr_in)));
 
-        if( iBytesSent != iBytesToSend )
+        if(iBytesSent == SOCKET_ERROR || static_cast<size_t>( iBytesSent ) != uBytesToSend)
         {
             m_strError = "Was not able to send data";
+            closesocket(sock);
             return false;
         }
 
-        uTotalBytesSent += iBytesSent;
+        uTotalBytesSent += static_cast<size_t>(iBytesSent);
+        pData += iBytesSent;
     }
 
     closesocket( sock );
-
     return true;
 }
 
 const std::string gelf::UDPProtocol::getError() const
 {
-    return  m_strError;
+    return m_strError;
 }
